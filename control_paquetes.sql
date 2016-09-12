219e4e78cbe0f32bf6e53d649dcfebc745206a14
pmtempone <pmtempone@gmail.com>
2016-09-11 21:55:04 -0300

    Control Paquetes

---
+++ control_paquetes.sql
@@ -0,0 +1,9 @@
+﻿SELECT tpaquete2,count(numero_de_cliente)
+  FROM public.fct_prod_premium
+  group by tpaquete2;
+/*campo con todo 0*/
+
+SELECT tpaquete3,count(numero_de_cliente)
+  FROM public.fct_prod_premium
+  group by tpaquete3;
+/*campo con todo 0*/
