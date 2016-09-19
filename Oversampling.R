c745ff5602b7aa04bf7bc50788e734c435b6b771
pmtempone <pmtempone@gmail.com>
2016-09-19 16:01:32 -0300

    Oversampling v2

--- Oversampling.R
+++ Oversampling.R
@@ -1,5 +1,6 @@
 -----#librerias-----
-
+library(rpart)
+library(caret)
 
 ----#repetir clase minoritaria (oversampling)------
 
@@ -8,3 +9,61 @@ df.over <- df.over[rep(seq_len(nrow(df.over)), each=300),]
 test.rm <- df.over[which(grepl("\\.",rownames(df.over))),]
 abril.over <- rbind(abril_dataset[,1:170],test.rm)
 
+-----#definicion  funcion ganancia para nuestro problema----
+  
+  ganancia = function( probs, clases ){
+    suma = 0 ;
+    largo = length( clases ) ;
+    
+    for( i in 1:largo )
+    {
+      if( probs[ i, "BAJA+2"]  > ( 250/(8000/300))   ){ suma <- suma + if( clases[i]=="BAJA+2" ) { 7750 } else { -250 }  
+      } ;
+    }
+    
+    return( suma )
+  }
+
+
+
+vcp <- 0 ;
+vminsplit <- 50 ;
+vminbucket <- 8 ;
+vmaxdepth <- 8
+
+
+for(  vmaxdepth  in  3:12 )
+{
+  semilla <- c( 102191, 200177, 410551, 552581, 892237 )
+  ganancias <- c() 
+  tiempos <- c()
+  
+  for( s in  1:5 )
+  {
+    # Genero training y testing con 70% , 30%
+    set.seed( semilla[s] )
+    abril_inTraining <- createDataPartition( abril.over$clase, p = .70, list = FALSE)
+    abril_dataset_training <- abril.over[ abril_inTraining,]
+    abril_dataset_testing  <- abril.over[-abril_inTraining,]
+    
+    
+    # generacion del modelo sobre los datos de training
+    t0 =  Sys.time()
+    abril_modelo  <- rpart( clase ~ .   ,   data = abril_dataset_training,   cp=vcp, minsplit=vminsplit, minbucket=vminbucket, maxdepth=vmaxdepth )  
+    t1 = Sys.time()
+    tiempos[s] <-  as.numeric(  t1 - t0, units = "secs" )
+    
+    
+    abril_testing_prediccion  = predict(  abril_modelo, abril_dataset_testing , type = "prob")
+    
+    
+    # calculo la ganancia normalizada  en testing
+    ganancias[s] = ganancia( abril_testing_prediccion,  abril_dataset_testing$clase ) / 0.30
+    
+    
+  }
+  
+  
+  cat( format(Sys.time(), "%Y%m%d %H%M%S"), "201604.txt", "rpart", vcp , vminsplit, vminbucket, vmaxdepth, mean(ganancias), mean(tiempos), ganancias , "\n", sep="\t", file="salida.txt", fill=FALSE, append=TRUE )
+  
+}
